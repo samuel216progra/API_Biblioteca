b080414aaefba6decaebe35586963322f3b06026
samuel216progra <srodriguez-2022165@kinal.edu.gt>
2024-03-05 16:55:43 -0600

    se creo usuario

---
+++ src/user/user.model.js
@@ -0,0 +1,35 @@
+import mongoose from 'mongoose';
+
+const UserSchema = mongoose.Schema({
+
+
+    
+    name: {
+        type: String,
+        required: [true, "The name is required"]
+    },
+    userName: {
+        type: String,
+        required: [true, "The name is required"]
+    },
+    lastName: {
+        type: String,
+        required: [true, "The last name is required"]
+    },
+    email: {
+        type: String,
+        required: [true, "The emmail is required"],
+        unique: true
+    },
+    password: {
+        type: String,
+        required: [true, "The password is required"]
+    },
+    state: {
+        type: Boolean,
+        default: true
+    }
+})
+
+
+export default mongoose.model('User', UserSchema);
